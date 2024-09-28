60af1c0e7ad7b48bf6a7b12136150f82cefd6f44
unknown <haroldaguirre1991@gmail.com>
2024-09-28 12:15:31 -0500

    cambio de la app darkmode

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import{TodoList}from './TodoList';
 import{TodoItem}from './TodoItem';
 import{CreateTodoButton}from './CreateTodoButton';
 const defaultTodos=[
-  {text:'Cortar cebolla',completed:true},
-  {text:'Tomar el Curso de Intro a React.js',completed:false},
-  {text:'Llorar con la Llorona',completed:false},
-  {text:'LALALALALA',completed:false},
+  {text:'Estudiar',completed:true},
+  {text:'Trabajar',completed:false},
+  {text:'Ejercicio',completed:false},
+  {text:'Dormir',completed:false},
 ];
 function App(){
   return(
   <>
-  <TodoCounter completed={16}total={25}/><TodoSearch/>
+  <TodoCounter completed={1}total={5}/><TodoSearch/>
   <TodoList>{defaultTodos.map(todo=>(
     <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>))}
   </TodoList>
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,4 +1,4 @@
 import './TodoSearch.css';function TodoSearch(){return(<input
-   placeholder="Cortar cebolla"
+   placeholder="Buscar Tarea..."
    className="TodoSearch"/>);}
    export{TodoSearch};
