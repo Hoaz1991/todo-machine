89193070c0dcb28e2aae9aa5fc1cbd43c4fa97b0
unknown <haroldaguirre1991@gmail.com>
2024-09-29 23:08:29 -0500

    Eventos en React: onClick, onChange

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,5 +1,11 @@
 
 import './CreateTodoButton.css';
-function CreateTodoButton(){return(<button className="CreateTodoButton">+</button>);}
+function CreateTodoButton(){return(<button className="CreateTodoButton" onClick={
+    (event) => {
+    console.log('le diste clic') 
+    console.log(event)
+    console.log(event.target)
+      }
+}>+</button>);}
 export{CreateTodoButton};
   
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,4 +1,14 @@
 import './TodoSearch.css';function TodoSearch(){return(<input
    placeholder="Buscar Tarea..."
-   className="TodoSearch"/>);}
+   className="TodoSearch"
+   onChange={(event)=>{
+      console.log('escribiste en el todo search')
+       console.log(event);
+       console.log(event.target);
+       console.log(event.target.value);
+       
+   }}
+   />
+);
+}
    export{TodoSearch};
