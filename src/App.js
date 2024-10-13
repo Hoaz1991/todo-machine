da66adada1e1b2f0966a80a3947c31e9a8c34ab8
unknown <haroldaguirre1991@gmail.com>
2024-10-12 23:19:57 -0500

    completando y eliminando TODOs

--- src/App.js
+++ src/App.js
@@ -28,8 +28,25 @@ function App(){
    );
 
 
-
+ 
   //  console.log('Los usuarios buscan: ' + searchValue)
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+   
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
    
   return(
   <>
@@ -46,6 +63,8 @@ function App(){
     key={todo.text} 
     text={todo.text} 
     completed={todo.completed}
+    onComplete={()=>completeTodo(todo.text)}
+    onDelete={()=>deleteTodo(todo.text)}
     />
     ))}
   </TodoList>
